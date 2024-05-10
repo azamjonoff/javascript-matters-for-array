975ff9f8194ae735f9631dee437dad9b153299fa
azamjonoff <abdullohazamjonov57gmail.com>
2024-05-10 22:15:20 +0500

    js matters add

---
+++ app.js
@@ -0,0 +1,94 @@
+// UYGA VAZIFA
+
+// 1)
+
+// const raqamlar = "34567"
+//   .split("")
+//   .reverse()
+//   .map((item) => {
+//     return Number(item);
+//   });
+
+// console.log(raqamlar);
+
+// 2)
+
+// if (2 == "2") {
+//   console.log("True");
+// } else console.log("False");
+// Bunda faqat qiymatlarni tekshiradi.
+
+// if (2 === "2") {
+//   console.log("True");
+// } else console.log("False");
+// Bunda malumot turi qiymati hammasini tekshiradi.
+
+// 3)
+
+// 5)
+
+// function stringFilter() {}
+
+// 6)
+
+// let first = [2, 4];
+// let second = [3, 1];
+// let arrBracket = [];
+
+// function twoArray() {
+//   let ikkiArr = first.concat(second);
+//   for (let i = 0; i < ikkiArr.length; i++) {
+//     let result = ikkiArr.forEach(i);
+//   }
+//   console.log(result);
+// }
+
+// twoArray();
+
+// 7)
+
+// let soz = "bAbdullohs";
+
+// function stringLastStart(word) {
+//   let boshharf = soz.substring(0, 1);
+//   let oxirgiharf = soz.substring(-1, -2);
+//   let result =
+//   console.log(boshharf);
+// }
+
+// stringLastStart();
+
+// 8)
+
+// let array = [2, 3, 6, 7];
+// let arrBracket = [];
+
+// function reverse(arr) {
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     arrBracket.push(array[i]);
+//   }
+//   console.log(arrBracket);
+// }
+
+// reverse();
+
+// 9)
+
+// const raqamlar = `3720`;
+
+// function ifLastZero(zero) {
+//   let newRaqam = raqamlar.slice(length, length - 1);
+//   console.log(newRaqam);
+// }
+
+// ifLastZero();
+
+// 10)
+
+// const raqamlar = [2, 4, 7];
+
+// function engKichigi(arr) {
+//   for (let i = 0; i < raqamlar.length; i++) {}
+// }
+
+// engKichigi();
